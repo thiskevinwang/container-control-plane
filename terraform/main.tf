daee798d6e5db0c911e00c8e693e1a61667e9f45
Kevin Wang <kwangsan@gmail.com>
2023-10-08 00:33:09 -0400

    update `main.tf`

--- terraform/main.tf
+++ terraform/main.tf
@@ -87,19 +87,20 @@ resource "aws_security_group" "allow_all" {
   tags = local.tags
 }
 
+# TODO(kevinwang): Side quest to understand EBS usage
+# resource "aws_ebs_volume" "shared-storage" {
+#   availability_zone = "us-east-1a"
+#   size              = 10
 
-resource "aws_ebs_volume" "shared-storage" {
-  availability_zone = "us-east-1a"
-  size              = 10
+#   tags = local.tags
+# }
 
-  tags = local.tags
-}
-
-resource "aws_volume_attachment" "ebs_att" {
-  device_name = "/dev/sdh"
-  volume_id   = aws_ebs_volume.shared-storage.id
-  instance_id = aws_instance.nomad-leader.id
-}
+# TODO(kevinwang): Side quest to understand EBS usage
+# resource "aws_volume_attachment" "ebs_att" {
+#   device_name = "/dev/sdh"
+#   volume_id   = aws_ebs_volume.shared-storage.id
+#   instance_id = aws_instance.nomad-leader.id
+# }
 
 resource "aws_instance" "nomad-leader" {
   ami                    = var.ami
@@ -114,11 +115,20 @@ resource "aws_instance" "nomad-leader" {
 # Outputs #
 ###########
 
-output "nomad_addr" {
-  value       = "export NOMAD_ADDR = http://${aws_instance.nomad-leader.public_ip}:4646"
+output "nomad_acl_bootstrap_reminder" {
+  value       = <<EOT
+# Reminder to bootstrap ACLs on initial launch
+nomad acl bootstrap
+EOT
+  description = "convenience command to bootstrap ACLs"
+}
+
+output "nomad_addr_cmd" {
+  value       = "export NOMAD_ADDR=http://${aws_instance.nomad-leader.public_ip}:4646"
   description = "convenience command to set NOMAD_ADDR"
 }
 
-output "ssh_string" {
-  value = "ec2-user@${aws_instance.nomad-leader.public_dns}"
+output "ssh_cmd" {
+  value       = "ssh -i ~/Downloads/test.pem ec2-user@${aws_instance.nomad-leader.public_dns}"
+  description = "convenience command to ssh into the instance"
 }
